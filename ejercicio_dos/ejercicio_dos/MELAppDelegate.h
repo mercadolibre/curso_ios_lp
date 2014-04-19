dfd5e1651a3415d2a28f61253422324a3ff3a9ff
Lorena Pressiani <lpressiani@mac-lpressiani.local>
2014-04-19 13:06:27 -0300

    ejercicio 2

---
+++ ejercicio_dos/ejercicio_dos/MELAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MELAppDelegate.h
+//  ejercicio_dos
+//
+//  Created by Lorena Pressiani on 4/19/14.
+//  Copyright (c) 2014 Lorena Pressiani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MELAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ejercicio_dos/ejercicio_dos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ejercicio_dos
+//
+//  Created by Lorena Pressiani on 4/19/14.
+//  Copyright (c) 2014 Lorena Pressiani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MELAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MELAppDelegate class]));
+    }
+}
